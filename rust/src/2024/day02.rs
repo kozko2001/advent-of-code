ffca3cdc3004aed6f8b55a34db432597f8c8c9e7
Jordi Coscolla <jordi@coscolla.net>
2024-12-05 23:06:45 +0100

    day 2024/02 rust

--- rust/src/2024/day02.rs
+++ rust/src/2024/day02.rs
@@ -12,8 +12,8 @@ fn main() {
     let r = solve_part1(&input);
     println!("result part 1: {}", r);
 
-    // let r = solve_part2(&input);
-    // println!("result part 2: {}", r);
+    let r = solve_part2(&input);
+    println!("result part 2: {}", r);
 }
 
 fn read_file_to_string(file_path: &str) -> Result<String, std::io::Error> {
@@ -21,49 +21,74 @@ fn read_file_to_string(file_path: &str) -> Result<String, std::io::Error> {
     fs::read_to_string(path)
 }
 
-fn solve_part1(input: &str) -> i32 {
+fn solve_part1(input: &str) -> usize {
     let puzzle = parse_input(input);
+    let p = puzzle
+        .iter()
+        .map(|line| {
+            let diffs: Vec<i32> = line
+                .windows(2)
+                .map(|x| (x[1] as i32) - (x[0] as i32))
+                .collect();
+            let is_monotonic = diffs.iter().all(|&x| x >= 0) || diffs.iter().all(|&x| x <= 0);
+            let has_valid_jumps = diffs.iter().all(|x| x.abs() <= 3 && x.abs() >= 1);
 
-    let p = puzzle.iter()
-        .map(|x| x)
-        .into();
-    println!("{}", p);
-
-    // let (mut l1, mut l2) = parse_input(input);
-    // l1.sort();
-    // l2.sort();
-    // zip(l1, l2).map(|(a, b)| b - a).map(|x| x.abs()).sum()
-    0
+            is_monotonic && has_valid_jumps
+        })
+        .filter(|&x| x)
+        .count();
+    p
 }
 
-fn solve_part2(input: &str) -> i32 {
-    // let (l1, l2) = parse_input(input);
-    // let count = l2.into_iter().fold(HashMap::new(), |mut map, item| {
-    //     *map.entry(item).or_insert(0) += 1;
-    //     map
-    // });
-    //
-    // l1.into_iter()
-    //     .fold(0, |acc, i| acc + i * count.get(&i).unwrap_or(&0))
-    0
+fn remove_one_combinations<T: Clone>(vec: &[T]) -> Vec<Vec<T>> {
+    (0..vec.len())
+        .map(|i| {
+            vec.iter()
+                .enumerate()
+                .filter(|(j, _)| *j != i)
+                .map(|(_, x)| x.clone())
+                .collect()
+        })
+        .collect()
 }
+fn solve_part2(input: &str) -> usize {
+    let puzzle = parse_input(input);
+    let p = puzzle
+        .iter()
+        .map(|line| {
+            let all_perms = remove_one_combinations(line);
 
+            let x = all_perms.into_iter().any(|line| {
+                let diffs: Vec<i32> = line
+                    .windows(2)
+                    .map(|x| (x[1] as i32) - (x[0] as i32))
+                    .collect();
+                let is_monotonic = diffs.iter().all(|&x| x >= 0) || diffs.iter().all(|&x| x <= 0);
+                let has_valid_jumps = diffs.iter().all(|x| x.abs() <= 3 && x.abs() >= 1);
 
+                is_monotonic && has_valid_jumps
+            });
+            x
+        })
+        .filter(|&x| x)
+        .count();
+    p
+}
 
 fn number(input: &str) -> IResult<&str, u32> {
-    return map_res(digit1, str::parse)(input)
+    map_res(digit1, str::parse)(input)
 }
 
 fn line(input: &str) -> IResult<&str, Vec<u32>> {
-    return separated_list1(space1, number)(input)
+    separated_list1(space1, number)(input)
 }
 
 fn matrix(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
-    return separated_list1(newline, line)(input)
+    separated_list1(newline, line)(input)
 }
 
 fn parse_input(input: &str) -> Vec<Vec<u32>> {
-    let p: IResult<&str, Vec<Vec<u32>>> = matrix(input);
+    let p = matrix(input);
     let (_, a) = p.unwrap();
     a
 }
@@ -79,29 +104,28 @@ mod tests {
 8 6 4 4 1
 1 3 6 7 9";
 
-
     #[test]
     fn test_part1() {
         let result = solve_part1(TEST_INPUT);
-        assert_eq!(result, 11);
+        assert_eq!(result, 2);
     }
 
     #[test]
     fn test_part2() {
         let result = solve_part2(TEST_INPUT);
-        assert_eq!(result, 31);
+        assert_eq!(result, 4);
     }
-
-    #[test]
-    fn test_parsing_input() {
-        let expected = vec![
-            vec![7, 6, 4, 2, 1],
-            vec![1, 2, 7, 8, 9],
-            vec![9, 7, 6, 2, 1],
-            vec![1, 3, 2, 4, 5],
-            vec![8, 6, 4, 4, 1],
-            vec![1, 3, 6, 7, 9],
-        ];
-        assert_eq!(parse_input(TEST_INPUT), expected);
-    }
+    //
+    // #[test]
+    // fn test_parsing_input() {
+    //     let expected = vec![
+    //         vec![7, 6, 4, 2, 1],
+    //         vec![1, 2, 7, 8, 9],
+    //         vec![9, 7, 6, 2, 1],
+    //         vec![1, 3, 2, 4, 5],
+    //         vec![8, 6, 4, 4, 1],
+    //         vec![1, 3, 6, 7, 9],
+    //     ];
+    //     assert_eq!(parse_input(TEST_INPUT), expected);
+    // }
 }
